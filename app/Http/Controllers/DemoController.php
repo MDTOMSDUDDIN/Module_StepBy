77e9a775ad5fbabdcf123c120d404a5d0739be9a
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2024-12-29 22:04:51 +0600

    retreving single value Frist()|find()|pluck()

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -7,8 +7,23 @@ use Illuminate\Support\Facades\DB;
 
 class DemoController extends Controller
 {
+    // public function DemoAction(){
+    //     $result=DB::table('profile')->get();             //get() all row select
+    //     return $result;
+    // }
+
+    // public function DemoAction(){
+    //     $result=DB::table('profile')->find(4);           //find()
+    //     return $result;
+    // }
+
+    // public function DemoAction(){
+    //     $result=DB::table('profile')->pluck('firstname');            //pluck single column value 
+    //     return $result;
+    // }
+
     public function DemoAction(){
-        $result=DB::table('profile')->get();
+        $result=DB::table('profile')->pluck('email',"firstname");
         return $result;
     }
     
