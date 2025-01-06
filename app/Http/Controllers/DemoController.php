f3ac0b7af7b610a97fe7a445b3656aa599412159
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-01-06 22:26:13 +0600

    retrivig |select|where clause|aggregate

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\Product;
+use App\Models\ProductDetail;
 use Illuminate\Http\Request;
 
 class DemoController extends Controller
@@ -29,4 +31,50 @@ class DemoController extends Controller
         return Brand::where('id','=',$request->id)
         ->delete();
     }
+
+    public function increment_decrement(Request $request){
+        return Product::where('id',"=",$request->id)
+        // ->increment('price',100);
+        // ->increment('price');
+        ->decrement('price',200);
+    }
+
+    public function retrive(Request $request){
+        // return Product::get();
+        // return Product::all();
+        // return Product::first();
+        // return Product::find(2);
+        // return Product::pluck('discount');
+        return Product::pluck('discount','image');
+
+    }
+
+    public function Aggreagates(Request $request){
+        // return Product::sum('price');
+        // return Product::avg('price');
+        // return Product::max('price');
+        // return Product::min('price');
+        return Product::count('price');
+    }
+    
+    public function select(Request $request){
+        // return Product::select('title')->get();
+        return Product::select('title')->distinct()->get();
+    }
+    public function where(Request $request){
+        // return Product::where('price','=','200')->get();
+        // return Product::where('price','<','200')->get();
+        // return Product::where('price','<=','300')->get();
+        // return Product::where('price','>=','1499.99')->get(); 
+        // return ProductDetail::where('des','LIKE','%running%')->get();       
+        // return ProductDetail::where('des','NOT LIKE','%running%')->get();   
+        // return Product::whereBetween('price',[1,500])->get();    
+        // return Product::whereIn('price',[1,902.99])->get();    
+        // return Product::whereNotIn('price',[1,902.99])->get();    
+        // return Product::whereNull('price')->get();    
+        // return Product::whereNotNUll('price')->get();    
+        // return Product::whereDay('updated_at','2025-01-12')->get();    
+        return Product::whereMonth('updated_at','01')->get();    
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('/create-brand',[DemoController::class,'create']);
 Route::post('/update-brand/{id}',[DemoController::class,'update']);
 Route::post('/create_update-brand/{brandName}',[DemoController::class,'create_update']);
-Route::post('/delete/{id}',[DemoController::class,'delete']);
+Route::get('/delete/{id}',[DemoController::class,'delete']);
+
+Route::get('/increment-Decrement/{id}',[DemoController::class,'increment_decrement']);
+Route::get('/retrive',[DemoController::class,'retrive']);
+Route::get('/Aggreagates',[DemoController::class,'Aggreagates']);
+Route::get('/select',[DemoController::class,'select']);
+Route::get('/where',[DemoController::class,'where']);
